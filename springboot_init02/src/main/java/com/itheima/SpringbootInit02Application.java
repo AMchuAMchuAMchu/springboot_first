ff81f2bb72b4af8c7688dd5493ac2cfb96a00f1d
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-16 08:41:01 +0800

    AM 01st commit

--- springboot_init02/src/main/java/com/itheima/SpringbootInit02Application.java
+++ springboot_init02/src/main/java/com/itheima/SpringbootInit02Application.java
@@ -1,4 +1,4 @@
-package com.itheima.springboot_init02;
+package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
