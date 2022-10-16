95c23deb93683f34570083f876d16f088b1a57d0
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-16 08:20:23 +0800

    AM 01st commit

---
+++ springboot_init02/src/main/java/com/itheima/controller/AnimeInfoController.java
@@ -0,0 +1,28 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_first
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-16 08:17:09
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@RequestMapping("/animeInfo")
+@RestController
+public class AnimeInfoController {
+
+    @GetMapping
+    public String getInfo(){
+
+
+
+
+
+    }
+
+}
